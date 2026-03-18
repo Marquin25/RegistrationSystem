f29c90161fe295f261bf95bdfd0d8bcadb815692
Marquin25 <marcusantoniopina@gmail.com>
2026-03-18 10:44:37 -0300

    Colocamos nossa primeira rota (/boasVindas) e aprendemos uma pouco sobre @RestController e @RequestMapping

---
+++ src/main/java/dev/java10x/registrationsystem/Controller.java
@@ -0,0 +1,18 @@
+package dev.java10x.registrationsystem;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController//TODO: Ele vai mapear esse arquivo do java (Controller.java) fazendo q ele vai avisar para o Java q isso é um Controller
+@RequestMapping // TODO: É para colocar todas as rotas no mesmo local
+public class Controller {
+
+    @GetMapping("/boasVindas") //TODO: Vai puxar/pegar informações / localhost:8080/boasVindas
+   // @PostMapping //TODO: Vai mandar informações
+   // @PutMapping //TODO: Vai alterar informações
+   // @PatchMapping //TODO: Vai alterar informações
+   // @DeleteMapping //TODO: Vai deletar as informaçoes
+    public String boasVindas() {
+        return "Hello World";
+    }
+
+}
