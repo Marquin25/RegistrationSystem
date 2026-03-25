deb852ef052b967c98624e54deb1f5f72542a607
Marquin25 <marcusantoniopina@gmail.com>
2026-03-25 10:28:42 -0300

    feat: Adicionando a minha nova interface UsuarioRepository

--- src/main/java/dev/java10x/registrationsystem/Usuarios/UsuarioRepository.java
+++ src/main/java/dev/java10x/registrationsystem/Usuarios/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package dev.java10x.registrationsystem.Usuarios;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Essa extensão (JpaRepository) é um simplificador, quando a gnt usa os bancos de dados
+// Qual classe que o JPA vai querer q fique escaneando as nossas entidades e criando tabelas no nosso banco de dados:
+// MissoesModel e o Long é por causa do id
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+
+
 }
