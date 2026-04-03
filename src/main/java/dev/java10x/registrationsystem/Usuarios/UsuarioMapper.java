51e63dc7cb40ec75c3c23d8873972adc0e70d08d
Marquin25 <marcusantoniopina@gmail.com>
2026-04-03 10:00:50 -0300

    Chore: Inclui a versao DTO pela entidade e mapeia manualmente no Ninja Mapper os caminhos entidade x dto

--- src/main/java/dev/java10x/registrationsystem/Usuarios/UsuarioMapper.java
+++ src/main/java/dev/java10x/registrationsystem/Usuarios/UsuarioMapper.java
@@ -1,4 +1,30 @@
 package dev.java10x.registrationsystem.Usuarios;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UsuarioMapper {
+
+    public UsuarioModel map(UsuarioDTO usuarioDTO) {
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setId(usuarioDTO.getId());
+        usuarioModel.setNome(usuarioDTO.getNome());
+        usuarioModel.setEmail(usuarioDTO.getEmail());
+        usuarioModel.setIdade(usuarioDTO.getIdade());
+        usuarioModel.setCpf(usuarioDTO.getCpf());
+        usuarioModel.setMissoes(usuarioDTO.getMissoes());
+
+        return usuarioModel;
+    }
+
+    public UsuarioDTO map(UsuarioModel usuarioModel) {
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(usuarioModel.getId());
+        usuarioDTO.setNome(usuarioModel.getNome());
+        usuarioDTO.setEmail(usuarioModel.getEmail());
+        usuarioDTO.setIdade(usuarioModel.getIdade());
+        usuarioDTO.setCpf(usuarioModel.getCpf());
+        usuarioDTO.setMissoes(usuarioModel.getMissoes());
+
+        return usuarioDTO;
+    }
 }
