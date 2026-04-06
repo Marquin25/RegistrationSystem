d3ed6b3c5df633195c863e0a65e9fa9b8d4a4710
Marquin25 <marcusantoniopina@gmail.com>
2026-04-06 09:15:20 -0300

    feat: implementação de CRUD completo para Missoes com DTO, Mapper, Service e Controller
    - Criada interface MissoesRepository com JpaRepository
    - Implementado MissoesService com métodos:
      - listarMissoes
      - criarMissao
      - alterarMissao
      - deletarMissao
    - Atualizado MissoesController para utilizar Service e DTO
    - Integração com MissoesMapper para conversão entre Model e DTO
    - Padronização das rotas REST (GET, POST, PUT, DELETE)
    
    Objetivo:
    Estruturar a API de Missoes seguindo boas práticas (camadas, DTO e separação de responsabilidades)

--- src/main/java/dev/java10x/registrationsystem/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/registrationsystem/Missoes/MissoesController.java
@@ -1,33 +1,40 @@
 package dev.java10x.registrationsystem.Missoes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController //TODO: Esta roteando a nossa API
-@RequestMapping("/missoes") //TODO: Mapear a nossa API
+import java.util.List;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
 
-    // GET: Mandar uma requisição para mostrar as missões (READ)
+    @Autowired
+    private MissoesService missoesService;
+
+    // GET - LISTAR
     @GetMapping("/listar")
-    public String listarMissao() {
-        return "Missao listada com sucesso";
+    public List<MissoesDTO> listarMissoes() {
+        return missoesService.listarMissoes();
     }
 
-    // POST: Mandar uma requisição para criar as missões (CREATE)
-    @PostMapping("/cria")
-    public String criarMissao() {
-        return "Missao criada com sucesso";
+    // POST - CRIAR
+    @PostMapping("/criar")
+    public MissoesDTO criarMissao(@RequestBody MissoesDTO dto) {
+        return missoesService.criarMissao(dto);
     }
 
-    // PUT: Mandar uma requisição para alterar as missões (UPDATE)
-    @PutMapping ("/alterar")
-    public String alterarMissao() {
-        return "Missao alterada com sucesso";
+    // PUT - ALTERAR
+    @PutMapping("/alterar/{id}")
+    public MissoesDTO alterarMissao(@PathVariable Long id,
+                                    @RequestBody MissoesDTO dto) {
+        return missoesService.alterarMissao(id, dto);
     }
 
-    // DELETE: Mandar uma requisição para deletar as missões (DELETE)
-    @DeleteMapping("/deletar")
-    public String deletarMissao() {
-        return "Missao deletada com sucesso";
+    // DELETE - DELETAR
+    @DeleteMapping("/deletar/{id}")
+    public String deletarMissao(@PathVariable Long id) {
+        missoesService.deletarMissao(id);
+        return "Missao deletada com sucesso!";
     }
-
 }
--- src/main/java/dev/java10x/registrationsystem/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/registrationsystem/Missoes/MissoesService.java
@@ -1,4 +1,46 @@
 package dev.java10x.registrationsystem.Missoes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class MissoesService {
+
+    @Autowired
+    private MissoesRepository missoesRepository;
+
+    @Autowired
+    private MissoesMapper missoesMapper;
+
+    // LISTAR
+    public List<MissoesDTO> listarMissoes() {
+        return missoesRepository.findAll()
+                .stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    // CRIAR
+    public MissoesDTO criarMissao(MissoesDTO dto) {
+        MissoesModel model = missoesMapper.map(dto);
+        MissoesModel salvo = missoesRepository.save(model);
+        return missoesMapper.map(salvo);
+    }
+
+    // ALTERAR
+    public MissoesDTO alterarMissao(Long id, MissoesDTO dto) {
+        MissoesModel model = missoesMapper.map(dto);
+        model.setId(id);
+
+        MissoesModel atualizado = missoesRepository.save(model);
+        return missoesMapper.map(atualizado);
+    }
+
+    // DELETAR
+    public void deletarMissao(Long id) {
+        missoesRepository.deleteById(id);
+    }
 }
