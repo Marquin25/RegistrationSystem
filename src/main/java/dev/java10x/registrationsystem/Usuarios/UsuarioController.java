10f18fb523053ceb59388b9606776aeb44653535
Marquin25 <marcusantoniopina@gmail.com>
2026-04-07 11:59:13 -0300

    refactor: Refatora o controller para incluir o ResponseEntity nas minhas rotas e customizar a resposta das requisições do cliente

--- src/main/java/dev/java10x/registrationsystem/Usuarios/UsuarioController.java
+++ src/main/java/dev/java10x/registrationsystem/Usuarios/UsuarioController.java
@@ -1,6 +1,8 @@
 package dev.java10x.registrationsystem.Usuarios;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,34 +32,67 @@ public class UsuarioController {
 
     // Adicionar um novo usuario (CREATE)
     @PostMapping("/criar")
-    public UsuarioDTO criarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
-        return usuarioService.criarUsuario(usuarioDTO) ;
+    public ResponseEntity<String> criarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        UsuarioDTO novoUsuario = usuarioService.criarUsuario(usuarioDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Usuario criado com sucesso: " + novoUsuario.getNome() + " (ID: " + novoUsuario.getId() + ")");
     }
 
     // Mostrar todos os usuarios (READ)
     @GetMapping("/listar")
-    public List<UsuarioDTO> listarUsuarios() {
-        return usuarioService.listarUsuarios();
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioService.listarUsuarios();
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.noContent().build(); // 204 - sem dados
+        }
+        return ResponseEntity.ok(usuarios); // 200 - OK
     }
 
     // Mostrar usuario por ID (READ)
     //@PathVariable serve para pegar o id e colocar na URL, dessa forma fica localhost:8080/lista/ e o id do usuario
     @GetMapping("/listar/{id}")
-    public UsuarioDTO listarUsuarioPorId(@PathVariable Long id) {
-        return usuarioService.listarUsuarioPorId(id);
+    public ResponseEntity<?> listarUsuarioPorId(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.listarUsuarioPorId(id);
+        if (usuario != null) {
+            return ResponseEntity.ok(usuario); // 200
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuário com ID " + id + " não encontrado"); // 404 - Não encontrado
+        }
     }
 
     // Alterar dados do usuario (UPDATE)
     @PutMapping("/alterar/{id}")
-    public UsuarioDTO alterarUsuarioPorId(@PathVariable Long id, @RequestBody UsuarioDTO usuario) {
-        return usuarioService.alterarUsuarioPorId(id, usuario);
+    public ResponseEntity<String> alterarUsuarioPorId(@PathVariable Long id, @RequestBody UsuarioDTO usuario) {
+
+        if (usuarioService.listarUsuarioPorId(id) != null) {
+            usuarioService.alterarUsuarioPorId(id, usuario);
+            return ResponseEntity.ok(
+                    "Usuario: " + usuario.getNome() + "\n" +
+                            "Email: " + usuario.getEmail() + "\n" +
+                            "ID: " + id + "\n" +
+                            "CPF: " + usuario.getCpf() + "\n" +
+                            "Missoes: " + usuario.getMissoes() + "\n" +
+                            "Alterado com sucesso!");
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuario com ID: " + id + " não encontrado");
+        }
     }
 
     // Deletar usuario (DELETE)
     //@PathVariable serve para pegar o id que o usuario colocar e colocar na URL, dessa forma fica localhost:8080/lista/ e o id do usuario
     @DeleteMapping("/deletar/{id}")
-    public void deletarUsuarioPorId(@PathVariable Long id) {
-        usuarioService.deletarUsuarioPorId(id);
+    public ResponseEntity<String> deletarUsuarioPorId(@PathVariable Long id) {
+
+        if (usuarioService.listarUsuarioPorId(id) != null) {
+            usuarioService.deletarUsuarioPorId(id);
+            return ResponseEntity.ok("Usuario com ID " + id + " deletado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Usuario com ID: " + id + " não encontrado");
+        }
     }
 
 }
